16d7038dd1a1c6432e2e64a4b07ef16708506dbf
Nathan Peek <nmp14fsu@gmail.com>
2021-02-14 22:11:41 -0500

    Rename file and reorganize for exporting

--- register.js
+++ register.js
@@ -4,13 +4,36 @@ const inquirer = require("inquirer");
 // Register
 const register = async () => {
     try {
+        const getUsername = () => {
+            return inquirer.prompt([
+                {
+                    name: "username",
+                    type: "input",
+                    message: "Enter username (between 4 and 16 characters)"
+                }
+            ])
+        }
+
+        const getPassword = () => {
+            return inquirer.prompt([
+                {
+                    name: "password",
+                    type: "password",
+                    message: "Enter password (between 4 and 16 characters)"
+                }
+            ])
+        }
+
         //Answers from inquirer functions
         const usernameAnswer = await getUsername();
+        //Make sure length of username is bettween 4 and 6
         if (usernameAnswer.username.length < 4 || usernameAnswer.username.length > 16) {
             console.log("username must be bewteen 4 and 16 characters.");
             return;
         }
+
         const passwordAnswer = await getPassword();
+        //Make sure length of password is bettween 4 and 6
         if (passwordAnswer.password.lengh < 4 || passwordAnswer.password.length > 16) {
             console.log("username must be bewteen 4 and 16 characters.");
             return;
@@ -19,22 +42,4 @@ const register = async () => {
     } catch (err) { console.log(err) }
 }
 
-const getUsername = () => {
-    return inquirer.prompt([
-        {
-            name: "username",
-            type: "input",
-            message: "Enter username (between 4 and 16 characters)"
-        }
-    ])
-}
-
-const getPassword = () => {
-    return inquirer.prompt([
-        {
-            name: "password",
-            type: "password",
-            message: "Enter password (between 4 and 16 characters)";
-        }
-    ])
-}
+module.exports.register;
