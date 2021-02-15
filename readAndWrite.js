bff584204e5f433d7d668d65a697a27e9e5d82fc
Nathan Peek <nmp14fsu@gmail.com>
2021-02-14 23:01:31 -0500

    add try and catch block to writeCSVFile method

--- readAndWrite.js
+++ readAndWrite.js
@@ -24,33 +24,37 @@ const ReadAndWrite = function () {
     //Function for generating CSV files. Will take in a path for where CSV should be written.
     //Usage will determine if it is for writting user info or pc info.
     this.writeCSVFile = (path, usage, data) => {
-        let csvWriter;
-        if (usage === "userInfo") {
-            csvWriter = createCsvWriter({
-                path: path,
-                header: [
-                    { id: 'username', title: 'Username' },
-                    { id: 'password', title: 'Password' },
-                    { id: 'uniqueID', title: 'UniqueID' },
-                ],
-                //apend instead of write
-                append: true
-            });
-        } else if (usage === "pcInfo") {
-            csvWriter = createCsvWriter({
-                path: path,
-                header: [
-                    { id: 'name', title: 'Name' },
-                    { id: 'surname', title: 'Surname' },
-                    { id: 'age', title: 'Age' },
-                    { id: 'gender', title: 'Gender' },
-                ]
-            });
-        }
+        try {
+            let csvWriter;
+            if (usage === "userInfo") {
+                csvWriter = createCsvWriter({
+                    path: path,
+                    header: [
+                        { id: 'username', title: 'Username' },
+                        { id: 'password', title: 'Password' },
+                        { id: 'uniqueID', title: 'UniqueID' },
+                    ],
+                    //apend instead of write
+                    append: true
+                });
+            } else if (usage === "pcInfo") {
+                csvWriter = createCsvWriter({
+                    path: path,
+                    header: [
+                        { id: 'name', title: 'Name' },
+                        { id: 'surname', title: 'Surname' },
+                        { id: 'age', title: 'Age' },
+                        { id: 'gender', title: 'Gender' },
+                    ]
+                });
+            }
 
-        csvWriter
-            .writeRecords(data)
-            .then(() => console.log('The CSV file was written successfully'));
+            csvWriter
+                .writeRecords(data)
+                .then(() => console.log('The CSV file was written successfully'));
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
