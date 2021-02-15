aaf3557592f411c336d7cc9752652f88ef081fae
Nathan Peek <nmp14fsu@gmail.com>
2021-02-14 22:35:59 -0500

    fix export of register

--- register.js
+++ register.js
@@ -24,22 +24,21 @@ const register = async () => {
             ])
         }
 
-        //Answers from inquirer functions
+        // Answers from inquirer functions
         const usernameAnswer = await getUsername();
-        //Make sure length of username is bettween 4 and 6
+        // Make sure length of username is bettween 4 and 6
         if (usernameAnswer.username.length < 4 || usernameAnswer.username.length > 16) {
             console.log("username must be bewteen 4 and 16 characters.");
             return;
         }
 
         const passwordAnswer = await getPassword();
-        //Make sure length of password is bettween 4 and 6
+        // Make sure length of password is bettween 4 and 6
         if (passwordAnswer.password.lengh < 4 || passwordAnswer.password.length > 16) {
             console.log("username must be bewteen 4 and 16 characters.");
             return;
         }
-        console.log("Retreived answers");
     } catch (err) { console.log(err) }
 }
 
-module.exports.register;
+module.exports = { register };
