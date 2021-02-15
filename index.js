3edded0e17c3252966e7f6afc8f722686288ec9f
Nathan Peek <nmp14fsu@gmail.com>
2021-02-14 22:05:55 -0500

    Add getPassword function. Add length propery to conditionsal of username as well

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ const register = async () => {
     try {
         //Answers from inquirer functions
         const usernameAnswer = await getUsername();
-        if (usernameAnswer.username < 4 || usernameAnswer.username > 16) {
+        if (usernameAnswer.username.length < 4 || usernameAnswer.username.length > 16) {
             console.log("username must be bewteen 4 and 16 characters.");
             return;
         }
         const passwordAnswer = await getPassword();
+        if (passwordAnswer.password.lengh < 4 || passwordAnswer.password.length > 16) {
+            console.log("username must be bewteen 4 and 16 characters.");
+            return;
+        }
         console.log("Retreived answers");
     } catch (err) { console.log(err) }
 }
 
-getUsername = () => {
+const getUsername = () => {
     return inquirer.prompt([
         {
             name: "username",
             type: "input",
             message: "Enter username (between 4 and 16 characters)"
+        }
+    ])
+}
+
+const getPassword = () => {
+    return inquirer.prompt([
+        {
+            name: "password",
+            type: "password",
+            message: "Enter password (between 4 and 16 characters)";
         }
     ])
 }
