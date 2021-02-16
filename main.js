6b3fd5b9c9f74bb0f96feb34e3554a216de5fada
Nathan Peek <nmp14fsu@gmail.com>
2021-02-16 04:12:03 -0500

    Add choice for accessing builds, check if folder exists in buildCheck and accessBuild

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const PCbuilder = require("./PCpartsObj");
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 const mainFunction = async (id) => {
-    await promptChoices();
+    await promptChoices(id);
 }
-
-const promptChoices = () => {
+// Prompt user for choices once logged on. User can check existing builds, access existing build, or make a new build.
+const promptChoices = (id) => {
     inquirer.prompt([
         {
             name: "userOptions",
@@ -14,21 +15,35 @@ const promptChoices = () => {
             choices: ["Check builds", "Access build", "Make build"]
         }
     ]).then(answer => {
-        if (answer.userOptions === "Check builds") buildCheck();
-        else if (answer.userOptions === "Access build") accessBuild();
-        else if (answer.userOptions === "Make build") buildMaker();
+        if (answer.userOptions === "Check builds") buildCheck(id);
+        else if (answer.userOptions === "Access build") accessBuild(id);
+        else if (answer.userOptions === "Make build") buildMaker(id);
     })
 }
 
-const buildCheck = () => {
-    console.log("build check");
+const buildCheck = (id) => {
+    if (!fs.existsSync(`./user_builds/${id}/`)) console.log("No builds found");
+    else {
+        // Access file in folder if it exists. List builds if any.
+    }
 }
 
-const accessBuild = () => {
-    console.log("access build");
+const accessBuild = (id) => {
+    inquirer.prompt([
+        {
+            name: "buildName",
+            type: "input",
+            message: "Which build would you like to access?"
+        }
+    ]).then(answer => {
+        if (!fs.existsSync(`./user_builds/${id}/`)) console.log("Cannot find any builds");
+        else {
+            // Read file, find build, return it.
+        }
+    })
 }
 
-const buildMaker = () => {
+const buildMaker = (id) => {
     console.log("make builds");
 }
 
