3720fcc9f95f9e0955854ff845c7b50fb8e312b7
Nathan Peek <nmp14fsu@gmail.com>
2021-02-15 22:00:27 -0500

    change readAndWrite to write

--- Write.js
+++ Write.js
@@ -1,26 +1,7 @@
-const csv = require("csv-parser");
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const fs = require("fs");
 
 // function for handling reading and writing PC data to CSV files.
-const ReadAndWrite = function () {
-    // Add function for reading from csv file.
-    this.readFile = (path) => {
-        try {
-            if (fs.existsSync(path)) {
-                fs.createReadStream(path)
-                    .pipe(csv())
-                    .on('data', (row) => {
-                        console.log(row);
-                    })
-                    .on('end', () => {
-                        console.log('CSV file successfully processed');
-                    });
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
+const Writer = function () {
     //Function for generating CSV files. Will take in a path for where CSV should be written.
     //Usage will determine if it is for writting user info or pc info.
     this.writeCSVFile = (path, usage, data) => {
@@ -58,4 +39,4 @@ const ReadAndWrite = function () {
     }
 }
 
-module.exports = ReadAndWrite;
+module.exports = Writer;
