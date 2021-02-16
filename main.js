e08c5b3c9cba51da69809b9c0443f2faf99005a8
Nathan Peek <nmp14fsu@gmail.com>
2021-02-16 03:57:43 -0500

    Add prompt for user choices after logging in and 3 functions based on choices (functions still need development)

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-const mainFunction = (id) => {
-    console.log(id);
+const PCbuilder = require("./PCpartsObj");
+const inquirer = require("inquirer");
+
+const mainFunction = async (id) => {
+    await promptChoices();
 }
 
+const promptChoices = () => {
+    inquirer.prompt([
+        {
+            name: "userOptions",
+            type: "list",
+            message: "Would you like to check existing builds, access an old build, or make a new build?",
+            choices: ["Check builds", "Access build", "Make build"]
+        }
+    ]).then(answer => {
+        if (answer.userOptions === "Check builds") buildCheck();
+        else if (answer.userOptions === "Access build") accessBuild();
+        else if (answer.userOptions === "Make build") buildMaker();
+    })
+}
+
+const buildCheck = () => {
+    console.log("build check");
+}
+
+const accessBuild = () => {
+    console.log("access build");
+}
+
+const buildMaker = () => {
+    console.log("make builds");
+}
 
 module.exports = { mainFunction }
