5e93e70d593a919316cb60ae9c9a12a353b86b30
Nathan Peek <nmp14fsu@gmail.com>
2021-03-07 22:11:32 -0500

    Create model for users table

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+
+User.init(
+    {
+        user_id: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+    sequelize,
+    modelName: "User",
+    timestamps: false
+});
+
+module.exports = User;
