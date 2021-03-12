b97cefda85f7d096cae93576d96a0ceaef1ddbb3
Nathan Peek <nmp14fsu@gmail.com>
2021-03-11 23:21:00 -0500

    Create register post request

--- register.js
+++ register.js
@@ -1,80 +1,52 @@
 const inquirer = require("inquirer");
 const nanoid = require("nanoid");
 
-const results = [];
-
-// Read current userInfo.
-const readData = async () => {
-    fs.createReadStream('./user_login_info/user-info.csv')
-        .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            //Call register function
-            register();
-        });
-}
-
-// Register
 const register = async () => {
-    try {
-        const userInfo = {}
-        let usernameExists = false;
+    // Get user input for username and password.
+    usernameAnswer = await getUsername();
+    passwordAnswer = await getPassword();
 
-        const getUsername = async () => {
-            return inquirer.prompt([
-                {
-                    name: "username",
-                    type: "input",
-                    message: "Enter username (between 4 and 16 characters)"
-                }
-            ])
-        }
+    const body = {
+        username: usernameAnswer.username,
+        password: passwordAnswer.password
+    }
 
-        const getPassword = async () => {
-            return inquirer.prompt([
-                {
-                    name: "password",
-                    type: "password",
-                    message: "Enter password (between 4 and 16 characters)"
-                }
-            ])
-        }
+    //fetch
+    const fetchResults = fetch("http://localhost:8000/", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    const resultsJSON = await (await fetchResults).json();
 
-        // Answers from inquirer functions
-        const usernameAnswer = await getUsername();
-        // Make sure length of username is bettween 4 and 6
-        if (usernameAnswer.username.length < 4 || usernameAnswer.username.length > 16) {
-            console.log("Username must be bewteen 4 and 16 characters.");
-            return;
-        }
+    // Wrong password or something went wrong
+    if (fetchResults.status === 401 || fetchResults.status === 500) {
+        console.log(`\n${resultsJSON.message}\n`);
+        return false;
+    }
 
-        const passwordAnswer = await getPassword();
-        // Make sure length of password is bettween 4 and 6
-        if (passwordAnswer.password.length < 4 || passwordAnswer.password.length > 16) {
-            console.log("Password must be bewteen 4 and 16 characters.");
-            return;
-        }
-        // Add username/password to userInfo Obj
-        userInfo.username = usernameAnswer.username;
-        userInfo.password = passwordAnswer.password;
-
-        // Check if username already exists.
-        results.forEach(obj => {
-            if (obj.Username === usernameAnswer.username) {
-                console.log("Username already exists!")
-                usernameExists = true;
-            }
-        })
-
-        // Generate uniqueID for user and add to userInfo obj
-        userInfo.uniqueID = nanoid.nanoid();
-
-        // Write (append) to CSV file
-        if (!usernameExists) {
-            const writer = new Writer;
-            writer.writeCSVFile("./user_login_info/user-info.csv", "userInfo", [userInfo])
-        }
-    } catch (err) { console.log(err) }
+    if (fetchResults.status === 201) {
+        loginStatus === true
+        return true;
+    }
 }
 
-module.exports = { readData };
+const getUsername = () => {
+    return inquirer.prompt([
+        {
+            name: "username",
+            message: "Enter username (4-16 characters): "
+        }
+    ])
+}
+
+const getPassword = () => {
+    return inquirer.prompt([
+        {
+            name: "password",
+            message: "Enter password (4-16 characters): "
+        }
+    ])
+}
+
+module.exports = register;
